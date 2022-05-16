43629a14f57f0d25f80595507d8dd0b54b3e77ec
Capad <Capad@users.noreply.github.com>
2022-05-16 09:49:30 +0200

    C# works like a charm

--- SeeShark/Program.cs
+++ SeeShark/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace SeeShark // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var addedNumbers = add_numbers(10, 5);
+            Console.WriteLine(addedNumbers);
+            Console.ReadLine();
+        }
+
+        [DllImport("rusted_sea.dll")]
+        private static extern Int32 add_numbers(Int32 arg1, Int32 arg2);
+    }
+}
