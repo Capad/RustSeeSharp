47dcef7d68960afc3e63d90e3eba2db2b07ba155
Capad <Capad@users.noreply.github.com>
2022-05-16 09:41:17 +0200

    adding first function in rust

--- rusted_sea/src/lib.rs
+++ rusted_sea/src/lib.rs
@@ -1,8 +1,16 @@
+#[no_mangle]
+pub extern fn add_numbers(arg1: i32, arg2: i32) -> i32 {
+    print!("Rust says: add_numbers({}, {})\n", arg1, arg2);
+    arg1 + arg2
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::add_numbers;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
+        let result: i32 = add_numbers(2, 2);
         assert_eq!(result, 4);
     }
 }
